a6cb2054c6517b8cd9515b822118f99d89696f3c
ivychow <treasureland730@gmail.com>
2025-03-31 23:19:33 +0800

    Add demo-UserInput files to main repo

---
+++ demo-UserInput/demo-UserInput/src/ExcerciseOne.java
@@ -0,0 +1,26 @@
+//Excercise on 31-Mar-2025
+//Calculate an area of rectangle
+
+import java.util.Scanner;
+
+public class ExcerciseOne {
+  public static void main(String[] args) {
+    
+    double width = 0;
+    double height = 0;
+    double area = 0;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("please input width: ");
+    width = scanner.nextDouble();
+
+    System.out.println("please input height: ");
+    height = scanner.nextDouble();
+
+    area = width * height;
+
+    System.out.println("The area is: " + area + "cm²");
+    scanner.close();
+  }
+}
